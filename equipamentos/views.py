822fa0bfa92048859b2411ab4b3b1a9e7f924247
Wagner <wagfrag@gmail.com>
2019-11-28 11:14:32 -0300

    Visualização de Dados

--- equipamentos/views.py
+++ equipamentos/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Equipamentos
 
 def equipamentos_list(request):
-	return render(request, 'equipamentos/equipamentos_list.html', {})
+	equipamentos = Equipamentos.objects.all().order_by('patrimonio')
+	return render(request, 'equipamentos/equipamentos_list.html', {'equipamentos' : equipamentos})
